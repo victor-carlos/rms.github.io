39afddcb31cfd14db51516da5e64817cd2960864
Victor Carlos <victor.carlos1395@gmail.com>
2018-07-01 14:27:19 -0300

    First pages commit

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $(".portfolio-item .img-fluid").click(function(){
-    $("#selected-photo").attr("src", $(this).attr("src"));
-    $("#galeria-focus").modal().hide().fadeIn("slow");
-  });
-});
